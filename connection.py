eb3849517316ed24a3e28812a1423a3cbbe14626
Tamas Frei <frei.tamas555@gmail.com>
2018-11-27 13:06:45 +0100

    select cards query added

--- connection.py
+++ connection.py
@@ -12,3 +12,13 @@ def select_boards_by_user(cursor, user_id):
     boards = cursor.fetchall()
     return boards
 
+@database_common.connection_handler
+def select_cards_by_board(cursor, user_id, board_id):
+    cursor.execute('''
+                    SELECT cards.id, cards.title, cards.status_id
+                    FROM cards
+                    WHERE cards.user_id = %(user_id)s AND cards.board_id = %(board_id)s;
+                    ''',
+                   {'user_id': user_id, 'board_id': board_id})
+    cards = cursor.fetchall()
+    return cards
