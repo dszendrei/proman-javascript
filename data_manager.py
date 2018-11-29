7c23f18774e0ca7632cc6f8ab8c22cdab3507855
Tamas Frei <frei.tamas555@gmail.com>
2018-11-29 16:37:15 +0100

    login added

--- data_manager.py
+++ data_manager.py
@@ -1,2 +1,15 @@
 import connection
+import hashing
 
+
+def user_login(username, password):
+    user = connection.select_user_by_username(username)
+    if user:
+        return hashing.verify_password(password, user['password'])
+    else:
+        return False
+
+
+def get_user_id(username):
+    user_id = connection.select_user_by_username(username)['id']
+    return user_id
--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, jsonify, redirect, url_for, flash
 import data_manager
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def boards():
+@app.route('/')
+def index():
     ''' this is a one-pager which shows all the boards and cards '''
-    return render_template('boards.html')
+    return render_template('index.html')
+
+
+@app.route('/boards')
+def boards():
+    user_id = session['user_id']
+    return render_template('boards.html', user_id=user_id)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if data_manager.user_login(username, password):
+        session['username'] = username
+        user_id = data_manager.get_user_id(session['username'])
+        session['user_id'] = user_id
+        session.pop('_flashes', None)
+        return redirect(url_for('boards'))
+    else:
+        flash(u'Wrong username or password.', 'login')
+        return redirect(url_for('index'))
+
+
+@app.route('/registration', methods=['POST'])
+def registration():
+    pass
 
 
 def main():
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
 
 
